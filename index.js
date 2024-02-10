41a5dbc16141a78a862fe5b80c874ac374982b36
abusayeds <abusayedstudent855@gmail.com>
2024-02-10 16:13:20 +0600

    api create

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send("task  server in raning ")
 })
+// 44nhE4ivRz6sY0bW
+// user
+    
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@atlascluster.pwovdtc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {serverApi: {version: ServerApiVersion.v1,strict: true,deprecationErrors: true, }
+});
 
+async function run() {
+    const userCollection = client.db('user').collection("taskUser")
+  try {
+    app.post('/user', async(req,res) => {
+       const data = req.body
+       const  result = await userCollection.insertOne(data) 
+       res.send(result)
+    })
+    app.get('/users', async(req ,res) =>{
+        const query = {}
+        const data =  userCollection.find(query)
+        const user = await data.toArray()
+        res.send(user)
+    })
+  } finally {
+ 
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`user task server raning on port ${port}`)
